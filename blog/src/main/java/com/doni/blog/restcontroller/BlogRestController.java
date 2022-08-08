e4c50f66d8209a1276cce2ae4a33a0e2827ef650
Doni <minjaebae@gmail.com>
2022-08-08 14:32:47 +0900

    아이디 중복체크 기능 적용

---
+++ blog/src/main/java/com/doni/blog/restcontroller/BlogRestController.java
@@ -0,0 +1,11 @@
+package com.doni.blog.restcontroller;
+
+import com.doni.blog.repository.UserRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BlogRestController {
+
+}
